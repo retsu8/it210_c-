5426402c3a8b667fcf901a9bbb99b9d38a6f1fd1
retsu8 <teck.geek1991@gmail.com>
2025-06-19 20:33:26 -0600

    Initial commit

---
+++ hello world module 1/hello_world.cpp
@@ -0,0 +1,14 @@
+// William Paddock
+// Simple Hello World
+// IT 210 Module 1
+
+// Get stream for the io write
+#include <iostream>
+
+// Main function to handle call c++
+int main() {
+    // Load the print statement into the std or std out to print to terminal
+    std::cout << "Hello World!";
+    // Close the script
+    return 0;
+}
---
+++ hello world module 1/hello_world.py
@@ -0,0 +1,6 @@
+# William Paddock
+# Simple Hello World
+# IT 210 Module 1
+
+#Prints hello world
+print("Hello World")
