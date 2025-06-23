5d7009c810686ea1cc00c4cdc93a68a8d937aa05
retsu8 <teck.geek1991@gmail.com>
2025-06-22 23:30:24 -0600

    adding in cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string str1;
+   string str2;
+   
+   getline(cin, str1);
+   getline(cin, str2);
+   
+   cout << str1 << endl;
+   cout << str2 << endl;
+ 
+   return 0;
+}
