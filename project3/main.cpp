a1d78a0d0a38367e3402781837a1a635f428b001
retsu <teck.geek1991@gmail.com>
2025-08-19 23:00:25 -0600

    forgot to add in cpp in header

--- project3/main.cpp
+++ project3/main.cpp
@@ -18,13 +18,23 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+
 class Inventory {
+	public:
+		Inventory();
+		Inventory(std::string name);
+		int InputFile(std::string inventory_file);
+		void SetStoreName(std::string name);
+		int GetInventory(string name);
+		void UpdateInventory(string name);
+		void PrintInventory();
 	private:
 		// Setting this to max inventory size
 		map<string, int> product;
-		std::string name = "";
+		std::string name;
 };
 
 Inventory::Inventory(){
@@ -36,38 +46,49 @@ Inventory::Inventory(std::string name){
 	this -> name = name;
 }
 
+void Inventory::PrintInventory(){
+	for (const auto& it : this -> product)
+	{
+	    std::cout << it.first << " " << it.second << endl;
+	}
+}
+
 void Inventory::SetStoreName(std::string name){
 	this -> name = name;
 }
 int Inventory::GetInventory(string name){
 	return this -> product[name];
 }
-void Inventory::UpdateInventory(string name, int count){
+void Inventory::UpdateInventory(string name){
 	map<string, int> local = this -> product;		
 	auto found = local.find(name);
 	if (found != local.end()){
-		found -> second += count;
+		local[name] = local[name] + 1;
 	} else {
-		local.insert({name, count});
+		local.insert({name, 1});
 	}
+
 	this -> product = local;
 }
 
-void Inventory::InputFile(std::string inventory_file){
+int Inventory::InputFile(std::string inventory_file){
 	// Open the file input stream
 	ifstream inFS;
-	inFS.open(file);
+	inFS.open(inventory_file);
 	if (!inFS.is_open()) {
       cout << "File is missing?" << endl;
       return 1; // 1 indicates error
    	}
-   	int count = 0
-    while (!inFS.fail()) {
-    	cout << "checking file: " << fileNum << endl;
-      	inFS >> fileNum;
+   	int count = 0;
+   	string line;
+   	 while (getline(inFS, line)) {
+        // Process each line as needed
+    	cout << "checking file: " << line << endl;
+    	UpdateInventory(line);
 		count += 1;
     }
 	cout << "Row count: " << count << endl;
+	return 0;
 
 }
 
@@ -77,6 +98,7 @@ int GroceryHandle(){
 	Inventory* grocer;
 	grocer = new Inventory(store_name);
 	grocer -> InputFile("CS210_Project_Three_Input_File.txt");
+	grocer -> PrintInventory();
 	return 0;
 }
 
