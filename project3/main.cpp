6d46ed484eddeeecde07f8243102211e481d111e
retsu <teck.geek1991@gmail.com>
2025-08-21 01:30:23 -0600

    forgot to add in cpp in header

--- project3/main.cpp
+++ project3/main.cpp
@@ -28,9 +28,14 @@ class Inventory {
 		Inventory(std::string name);
 		int InputFile(std::string inventory_file);
 		void SetStoreName(std::string name);
+		void PrintMenu(int choice);
 		int GetInventory(string name);
 		void UpdateInventory(string name);
+		void SearchInput();
+		int GetInput();
 		void PrintInventory();
+		void PrintFrequency();
+		void ToLower(std::string& lowerMe);
 	private:
 		// Setting this to max inventory size
 		map<string, int> product;
@@ -46,25 +51,55 @@ Inventory::Inventory(std::string name){
 	this -> name = name;
 }
 
+void Inventory::PrintMenu(int choice){
+	// Menu to print user input
+	switch(choice){
+		case 0:
+			cout <<  "Which item are you searching for?" << endl;
+			break;
+		case 1:
+			cout << "Choose from the following menu" << endl;
+			cout << "1. Search" << endl;
+			cout << "2. Inventory" << endl;
+			cout << "3. Frequency" << endl;
+			cout << "4. Exit" << endl;
+		}
+}
 void Inventory::PrintInventory(){
-	for (const auto& it : this -> product)
-	{
-	    std::cout << it.first << " " << it.second << endl;
+	// Iter through the map printing all the values in inventory
+	map<string,int>::iterator it; 
+	for (it=this->product.begin();it!=this->product.end();++it) {
+		string capital = it->first;
+		capital[0] = toupper(capital[0]);
+	    cout << it->second << " " << capital << endl;
 	}
 }
 
 void Inventory::SetStoreName(std::string name){
+	// Set the store name
 	this -> name = name;
 }
 int Inventory::GetInventory(string name){
+	// Return the product inventory
 	return this -> product[name];
 }
-void Inventory::UpdateInventory(string name){
-	map<string, int> local = this -> product;		
+
+void Inventory::ToLower(std::string& lowerMe) {
+	// Lowercase the string
+	for (size_t i = 0; i < lowerMe.length(); ++i) {
+        lowerMe[i] = tolower(lowerMe[i]);
+    }
+}
+
+void Inventory::UpdateInventory(std::string name){
+	// refernce the product
+	map<string, int> local = this -> product;
+	ToLower(name);
 	try{
+		// Update if exists
 		local[name] = local[name] + 1;
-		cout << local[name];
 	} catch (int e) {
+		// if not add a new one.
 		local.insert({name, 1});
 	}
 
@@ -73,23 +108,67 @@ void Inventory::UpdateInventory(string name){
 
 int Inventory::InputFile(std::string inventory_file){
 	// Open the file input stream
+	ifstream saveFS;
+	saveFS.open(this -> name + ".dat");
+	if(!saveFS.is_open()){
+		// if file not found create a saavefile
+		std::ofstream saveFS (this -> name + ".dat");
+	} else {
+		// Add the saved inventory to the file
+		string line;
+	   	while (getline(saveFS, line)) {
+	        // Process each line as needed
+	    	UpdateInventory(line);
+	    }
+	}
 	ifstream inFS;
 	inFS.open(inventory_file);
 	if (!inFS.is_open()) {
-      cout << "File is missing?" << endl;
       return 1; // 1 indicates error
    	}
-   	int count = 0;
    	string line;
-   	 while (getline(inFS, line)) {
+   	while (getline(inFS, line)) {
         // Process each line as needed
     	cout << "checking file: " << line << endl;
     	UpdateInventory(line);
-		count += 1;
     }
-	cout << "Row count: " << count << endl;
+	inFS.close();
+
 	return 0;
 
+}
+
+void Inventory::SearchInput(){
+	cout << "What would you like to search?" << endl;
+	string query;
+	getline(cin, query);
+	ToLower(query);
+	map<string, int> local = this -> product;
+	if (local.find(query) == local.end()) {
+		cout << "That item is not in the inventory" << endl;
+	} else {
+		cout << query << " " << local[query] << endl;
+	}
+}
+
+int Inventory::GetInput(){
+	int i = 0;
+	cin >> i;
+	while(!cin && (i < 0 || i >4)) // or if(cin.fail())
+	{
+	    cout << "Invalid Input; only enter a number between1 1 and 4";
+	    // user didn't input a number
+	    cin.clear(); // reset failbit
+	    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //skip bad input
+	    // next, request user reinput
+		cin >> i;
+	}
+	// Return the response if valid
+	return i;
+}
+
+void Inventory::PrintFrequency(){
+	cout << "Implment me" << endl;
 }
 
 int GroceryHandle(){
@@ -98,6 +177,22 @@ int GroceryHandle(){
 	Inventory* grocer;
 	grocer = new Inventory(store_name);
 	grocer -> InputFile("CS210_Project_Three_Input_File.txt");
+	int input = 0;
+	while(input != 4){
+		grocer -> PrintMenu(1);
+		input = grocer -> GetInput();
+		switch(input) {
+			case 1:
+				grocer -> SearchInput();
+				break;
+			case 2:
+				grocer -> PrintInventory();
+				break;
+			case 3:
+				grocer -> PrintFrequency();
+			}
+
+	}
 	grocer -> PrintInventory();
 	return 0;
 }
